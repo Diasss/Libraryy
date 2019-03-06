19b80fc9fbb02e75c437202ac81364663b1939de
Оспанов Диас <osp.dasik@mail.ru>
2019-03-06 17:52:15 +0600

    Добавьте файлы проекта.

---
+++ Library/Model/Libraryy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Model
+{
+    public class Libraryy
+    {
+        public Libraryy()
+        {
+            Console.WriteLine("Добро пожаловать!\n1)Зарегестрироваться\n2)Войти");
+
+        }
+    }
+}
